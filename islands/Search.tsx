28cc8284680fe2eee5f8dc861c503ee833e3fa53
ImMyst <titouan.galvani@yeap.ai>
2022-07-04 00:28:24 +0200

    fix: fixing search icon position

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -7,7 +7,7 @@ const Search = () => {
     return (
         <div class={tw`bg-accent rounded-xl font-main h-full`}>
             <form action="/" method="get" class={tw`flex items-center p-2 w-full h-full`}>
-                <div class={tw`mx-4`}>
+                <div class={tw`ml-4 mr-6 -mt-1`}>
                     <SearchIcon />
                 </div>
                 <input class={tw`border-none px-4 focus:outline-none focus:bg-primary h-12 rounded-xl mr-4 bg-transparent w-full`} type="text" placeholder="Search Github username..." />
