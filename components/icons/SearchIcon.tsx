7f0c64f01fe15f427340dcf4648c74e5284daeb7
ImMyst <titouan.galvani@yeap.ai>
2022-07-03 23:55:53 +0200

    fix: fixing seach icon color

--- components/icons/SearchIcon.tsx
+++ components/icons/SearchIcon.tsx
@@ -2,13 +2,9 @@
 import { h } from "preact";
 import { tw } from 'twind';
 
-interface Props {
-    color?: string;
-}
-
-export const SearchIcon = ({color}: Props) =>
+export const SearchIcon = () =>
 <div class={tw`w-5 h-5`}>
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" className={tw`h-5 w-5 search-icon`} viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
     </svg>
 </div>
--- islands/Search.tsx
+++ islands/Search.tsx
@@ -8,7 +8,7 @@ const Search = () => {
         <div class={tw`bg-accent rounded-xl font-main`}>
             <form method="get" class={tw`flex items-center p-2 w-full`}>
                 <div class={tw`mx-4`}>
-                    <SearchIcon color={'fill-main'} />
+                    <SearchIcon />
                 </div>
                 <input class={tw`bg-transparent w-full`} type="text" placeholder="Search Github username..." />
                 <button type="submit" class={tw`rounded-xl bg-main py-3 px-6 flex justify-end`}>Search</button>
